c08651401ba0c002f2de4da3b718459563a1cfa5
Huy Kha Duy <huykhaduy@gmail.com>
2022-03-22 17:00:51 +0700

    Add some comments

--- src/App.java
+++ src/App.java
@@ -6,7 +6,13 @@ import GUI.MainForm;
 public class App {
     public static void main(String[] args) throws Exception {
         new MainForm().setVisible(true);
-        
+
+        //Write your code here
+        //Show some thing great
+        //JFrame frame = new JFrame("Hello World");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setSize(300, 300);
+        //frame.setVisible(true);
 
     }
 }
