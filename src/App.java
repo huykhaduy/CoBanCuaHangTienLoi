e6cda129b75b2e216fbff9bb800774a90f3f9909
duy <duy3908@gmail.com>
2022-03-22 15:25:41 +0000

    Change create function calling

--- src/App.java
+++ src/App.java
@@ -5,7 +5,10 @@ import GUI.MainForm;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        new MainForm().setVisible(true);
+        // new MainForm().setVisible(true);
+
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
         //Hello form duy cute
         
         //Sometime we need to change
